2f98d23daa54978135bd2eee02d127a9dd47d76c
nirav <darjinirav18@gmail.com>
2021-07-08 15:48:21 +0530

    exercises are added

---
+++ exercise1.rb
@@ -0,0 +1,6 @@
+puts "Hello World."
+puts "Hello Again."
+puts "I like typing this."
+puts "Yay ! Printing."
+puts "I'd much rather than you 'not'."
+puts 'I "said" do not touch this.'
---
+++ exercise2.rb
@@ -0,0 +1,9 @@
+# A Comment , this is so you can read this program later.
+#Anthing after # is  ignored by the ruby
+
+puts "i could have code like this." # and the comment after is ignored
+
+# You can also use comment to "disable" or comment out a piece of code:
+# puts "this won't work"
+
+puts "this will run."
